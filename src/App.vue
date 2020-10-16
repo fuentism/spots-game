5228cd4c4f7c73f1db66f390d6719c62acb0eb3e
Krista Fuentes <fuentism@gmail.com>
2020-10-16 14:13:47 -0400

    Add a site title and utility nav area

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,47 @@
 <template>
   <div id="app">
+    <UtilityNav />
     <SpotsGame />
   </div>
 </template>
 
 <script>
-import SpotsGame from './components/SpotsGame.vue'
+import SpotsGame from './components/SpotsGame.vue';
+import UtilityNav from './components/UtilityNav.vue';
 
 export default {
   name: 'App',
   components: {
-    SpotsGame
+    SpotsGame,
+    UtilityNav
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  font-size: 100%;
+}
+h1 {
+  font-size: 2rem;
+}
+h1 {
+  font-size: 1.5rem;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 25px;
 }
 button {
   margin: 10px 4px;
   padding: 6px 10px;
   font-size: 15px;
   border: none;
+  border-radius: 3px;
   background: #ddd;
 }
 button:hover {
---
+++ src/components/UtilityNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="utility-nav">
+    <h1 class="site-title">Spots! A Memory Game.</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'UtilityNav',
+}
+</script>
+
+<style>
+.utility-nav {
+  margin-bottom: 1rem;
+  padding: 8px;
+  background-color: #883eb1;
+}
+
+.site-title {
+  margin: 0;
+  color: #fff;
+}
+</style>
